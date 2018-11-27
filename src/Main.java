9550d06eaeae9d13a60a68ac70a86096534edbc4
Łukasz Waręcki <glenski2be7@gmail.com>
2018-11-27 23:23:56 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(2147483647);
+        System.out.println(32);
+        System.out.println(1L);
+        Object number = 1L;
+        System.out.println(number.getClass());
+        System.out.println(-9223372036854775808L);
+        System.out.println(9223372036854775807L);
+        System.out.println(64);
+        Object number1 = 1;
+        System.out.println(number1.getClass());
+        System.out.println(-32768);
+        System.out.println(-128);
+        System.out.println(127);
+        System.out.println(8);
+        System.out.println(0);
+        System.out.println(65535);
+        System.out.println(16);
+        System.out.println(1000.0D);
+        System.out.println(1000.0D);
+        System.out.println(4.9E-324D);
+        System.out.println(1.7976931348623157E308D);
+        System.out.println(64);
+        System.out.println(1.0F);
+        System.out.println(1000.0F);
+        System.out.println(1000.0F);
+        System.out.println(1.4E-45F);
+        System.out.println(3.4028235E38F);
+        System.out.println(32);
+    }
+}
